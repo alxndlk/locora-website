df1929dabd3e5457b762ae9efba55fc957f99fd5
alxndlk <109311598+alxndlk@users.noreply.github.com>
2025-09-01 23:30:42 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ProfileProvider } from "../../context/ProfileContext";
+import { inter } from "@/fonts/fonts";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className={inter.className}>
         <ProfileProvider>{children}</ProfileProvider>
       </body>
     </html>
